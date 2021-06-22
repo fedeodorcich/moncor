5f9bd03fff33206ae585fe34e741e9dcc8e56a92
fedeodorcich <32882548+fedeodorcich@users.noreply.github.com>
2021-06-22 12:10:05 -0300

    last updated

--- sjbikes.sql
+++ sjbikes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-03-2021 a las 14:55:56
+-- Tiempo de generación: 22-06-2021 a las 17:08:52
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 8.0.0
 
@@ -40,12 +40,11 @@ CREATE TABLE `asociacion` (
 --
 
 INSERT INTO `asociacion` (`id`, `patente`, `user`, `entidad`, `timestamp`) VALUES
-(4, 'CWJ 287', 'Odin', 'Comisaria 5ta', '2021-03-09 22:53:54'),
-(5, 'CWJ 287', 'Thor', 'Comisaria 5ta', '2021-03-09 22:53:54'),
-(6, 'ABC 123', 'Odin', 'Comisaria 5ta', '2021-03-09 23:08:44'),
-(7, 'ABC 123', 'Thor', 'Comisaria 5ta', '2021-03-09 23:08:44'),
-(8, 'FNO992', 'Odin', 'Comisaria 5ta', '2021-03-13 05:23:48'),
-(9, 'FNO992', 'Thor', 'Comisaria 5ta', '2021-03-13 05:23:48');
+(8, 'CWJ287', 'fede.odorcich@gmail.com', 'Particular', '2021-04-23 12:52:04'),
+(9, 'WQE312', 'fede.odorcich@gmail.com', 'Particular', '2021-04-23 13:31:38'),
+(10, 'ASDASD', 'aaaaaa@gmail.com', 'Particular', '2021-04-23 14:34:47'),
+(11, 'MNH543', 'josep@gmail.com', 'Particular', '2021-04-26 22:48:47'),
+(12, 'CWJ287', 'fede.odorcich@gmail.com', 'Particular', '2021-04-28 13:55:24');
 
 -- --------------------------------------------------------
 
@@ -64,11 +63,10 @@ CREATE TABLE `entidades` (
 --
 
 INSERT INTO `entidades` (`id`, `nombre`, `timestamp`) VALUES
-(1, 'Comisaria 5ta', '2021-02-25 15:51:49'),
-(2, 'Valhalla', '2021-02-25 16:47:38'),
-(3, 'Lorencia', '2021-03-15 13:08:15'),
-(4, 'Devias', '2021-03-15 13:10:02'),
-(5, 'Lost Tower', '2021-03-15 13:10:10');
+(1, 'Particular', '2021-04-14 12:42:40'),
+(2, 'comisaria', '2021-04-20 22:28:24'),
+(3, 'Comisaria 29', '2021-04-21 21:47:32'),
+(4, 'Comisaria 5ta', '2021-04-26 22:46:26');
 
 -- --------------------------------------------------------
 
@@ -80,7 +78,9 @@ CREATE TABLE `especificaciones` (
   `id` int(255) NOT NULL,
   `vehiculo` varchar(50) COLLATE utf8mb4_spanish2_ci NOT NULL,
   `cilindrada` int(10) NOT NULL,
-  `km_service` int(10) NOT NULL,
+  `km_service` int(255) NOT NULL,
+  `km_cubiertas` int(255) NOT NULL,
+  `anio_bateria` int(100) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
@@ -88,12 +88,35 @@ CREATE TABLE `especificaciones` (
 -- Volcado de datos para la tabla `especificaciones`
 --
 
-INSERT INTO `especificaciones` (`id`, `vehiculo`, `cilindrada`, `km_service`, `timestamp`) VALUES
-(2, 'moto', 150, 210, '2021-03-10 15:19:17'),
-(3, 'moto', 125, 100, '2021-03-12 14:21:02'),
-(4, 'auto', 13, 234, '2021-03-12 16:13:19'),
-(6, 'Moto', 350, 200, '2021-03-12 16:37:21'),
-(7, 'Moto', 777, 888, '2021-03-13 03:47:35');
+INSERT INTO `especificaciones` (`id`, `vehiculo`, `cilindrada`, `km_service`, `km_cubiertas`, `anio_bateria`, `timestamp`) VALUES
+(1, 'Moto', 150, 4000, 1000, 3, '2021-04-23 14:17:13'),
+(2, 'Moto', 250, 13000, 2000, 2, '2021-04-20 22:35:19'),
+(3, 'Moto', 200, 4000, 3000, 3, '2021-04-21 21:51:33');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registros`
+--
+
+CREATE TABLE `registros` (
+  `id` int(255) NOT NULL,
+  `patente` varchar(100) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `fecha` varchar(20) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `registros`
+--
+
+INSERT INTO `registros` (`id`, `patente`, `fecha`, `timestamp`) VALUES
+(1, 'DRT786', '2021-04-20 12:00:00', '2021-04-21 21:51:02'),
+(2, 'CWJ287', '2021-04-23 12:00:00', '2021-04-23 12:16:27'),
+(3, 'CWJ287', '2021-04-23 12:00:00', '2021-04-23 12:21:41'),
+(4, 'CWJ287', '2021-04-23 12:00:00', '2021-04-23 12:23:01'),
+(5, 'MNH543', '2021-04-26 12:00:00', '2021-04-26 22:51:27'),
+(6, 'ASDASD', '2021-05-07 12:00:00', '2021-05-07 13:04:34');
 
 -- --------------------------------------------------------
 
@@ -114,7 +137,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `user`, `pass`, `timestamp`) VALUES
-(1, 'Walter Juarez', 'walter', '1234', '2021-02-23 15:29:52');
+(1, 'Usuario', 'user', 'pass', '2021-04-14 12:26:19'),
+(2, 'Javier Bustos', 'javier', 'Dylan353', '2021-04-21 21:53:30');
 
 -- --------------------------------------------------------
 
@@ -136,8 +160,9 @@ CREATE TABLE `users_vehiculos` (
 --
 
 INSERT INTO `users_vehiculos` (`id`, `nombre`, `telefono`, `email`, `entidad`, `timestamp`) VALUES
-(2, 'Odin', '2643453', 'fede.odorcich@gmail.com', 'Comisaria 5ta', '2021-02-25 16:48:27'),
-(19, 'Thor', '2643453', 'qweqweq@gmail.com', 'Comisaria 5ta', '2021-02-25 16:11:36');
+(1, 'Federico Odorcich', '2645094139', 'fede.odorcich@gmail.com', 'Particular', '2021-04-14 12:44:21'),
+(2, 'asdsds', '264345678', 'aaaaaa@gmail.com', 'Particular', '2021-04-21 21:48:09'),
+(3, 'jose perez', '2645678765', 'josep@gmail.com', 'Particular', '2021-04-26 22:47:13');
 
 -- --------------------------------------------------------
 
@@ -151,10 +176,15 @@ CREATE TABLE `vehiculos` (
   `patente` varchar(30) COLLATE utf8mb4_spanish2_ci NOT NULL,
   `cilindrada` int(11) NOT NULL,
   `km_total` int(255) NOT NULL,
+  `total_cubiertas` int(255) NOT NULL,
   `km_aux` int(255) NOT NULL,
+  `km_cubiertas` int(255) NOT NULL,
   `km_diarios` int(255) NOT NULL,
   `last_service` datetime NOT NULL,
+  `last_cubiertas` datetime NOT NULL,
+  `last_battery` datetime NOT NULL,
   `fecha_carga` datetime NOT NULL,
+  `notified` int(1) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
@@ -162,10 +192,9 @@ CREATE TABLE `vehiculos` (
 -- Volcado de datos para la tabla `vehiculos`
 --
 
-INSERT INTO `vehiculos` (`id`, `vehiculo`, `patente`, `cilindrada`, `km_total`, `km_aux`, `km_diarios`, `last_service`, `fecha_carga`, `timestamp`) VALUES
-(9, 'auto', 'CWJ287', 100, 0, 0, 150, '2021-03-09 19:53:54', '2021-03-09 19:53:54', '2021-03-13 05:36:06'),
-(10, 'auto', 'ABC 123', 0, 0, 0, 123, '2021-03-09 20:08:44', '2021-03-09 20:08:44', '2021-03-09 23:08:44'),
-(12, 'moto', 'FNO992', 150, 0, 0, 130, '2021-03-13 02:23:48', '2021-03-13 02:23:48', '2021-03-13 05:23:48');
+INSERT INTO `vehiculos` (`id`, `vehiculo`, `patente`, `cilindrada`, `km_total`, `total_cubiertas`, `km_aux`, `km_cubiertas`, `km_diarios`, `last_service`, `last_cubiertas`, `last_battery`, `fecha_carga`, `notified`, `timestamp`) VALUES
+(9, 'moto', 'WQE312', 150, 22300, 0, 3200, 2800, 200, '2021-04-21 10:31:38', '2021-04-23 10:31:38', '2021-04-23 10:31:38', '2021-04-23 10:31:38', 0, '2021-05-07 13:05:38'),
+(10, 'moto', 'ASDASD', 200, 16600, 0, 2100, 0, 150, '2021-04-23 11:34:47', '2021-05-07 12:00:00', '2021-04-23 11:34:47', '2021-04-23 11:34:47', 0, '2021-05-07 13:05:38');
 
 --
 -- Índices para tablas volcadas
@@ -187,6 +216,12 @@ ALTER TABLE `entidades`
 -- Indices de la tabla `especificaciones`
 --
 ALTER TABLE `especificaciones`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `registros`
+--
+ALTER TABLE `registros`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -215,31 +250,37 @@ ALTER TABLE `vehiculos`
 -- AUTO_INCREMENT de la tabla `asociacion`
 --
 ALTER TABLE `asociacion`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `entidades`
 --
 ALTER TABLE `entidades`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `especificaciones`
 --
 ALTER TABLE `especificaciones`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `registros`
+--
+ALTER TABLE `registros`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `users_vehiculos`
 --
 ALTER TABLE `users_vehiculos`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `vehiculos`
